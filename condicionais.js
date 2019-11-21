63ce5ab35f57ac9a00d7d4efa857f97ac09fecee
Tereza Niederauer <tete.leticia@gmail.com>
2019-11-20 23:36:53 -0300

    trabalho

---
+++ condicionais.js
@@ -0,0 +1,41 @@
+/*  
+ 1. Na porta de uma balada, confirme a seguintes informações:
+
+    SE for maior e igual a 18  - ( Passe a mensagem ao usuario que "Maior que 18 - autorizado")
+    Se for menor que 18 - ( Passe a mensagem ao usuario que "Menor que 18 - não autorizado")
+
+*/
+
+
+/*  
+ 2. Em uma fila de cadastro, eu tenho 25 anos e atendente precisa  confirmar a seguintes informações:
+
+    SE idade é menor igual a  18 -  ( Passe a mensagem ao usuario que "criança/adolescente")
+    SE for maior que 18  E menor igual a 60 - ( Passe a mensagem ao ususrio que "Adulto")
+    SE NÃO  ( Passe a mensagem ao ususrio que "Idoso")
+*/
+
+
+/* 
+    3. Calculo IMC
+
+    Crie uma variavel receba  peso em kg, uma variavel que receba altura em metros. 
+    Faça as comparações necessarias usando if e else if e els, para determinar qual a interpretação do IMC atual.
+
+    IMC  / Interpretação
+
+    Até 18,5 (inclusive)  / Abaixo do peso normal
+
+    De 18,5 a 25 (inclusive) / Peso normal
+
+    De 25 a 30 (inclusive) / Acima do peso normal
+
+    Acima de 30 / Obesidade
+
+    Exemplo:
+    Peso = 85 
+    Altura = 1.80
+    Resultado = Acima do peso normal 
+
+*/
+
