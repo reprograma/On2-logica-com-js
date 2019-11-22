75e6e42f7a696d7e0a73d637a796ee6b07d1bac8
Stéphanie Marchuk <stephmarchuk15@gmail.com>
2019-11-22 20:46:38 -0200

    Adicionando resolucao Stephanie

---
+++ exercicio-casa/stephanie-marchuk/resolucao - funcao.js
@@ -0,0 +1,86 @@
+/* 
+    1.  Crie uma função que receba três notas como parâmetro e retorne a média, indicando 
+        se a pessoa passou ou se foi reprovada;
+
+        Dica:
+        Nesse exercicio você vai usar a function e if e else juntos.
+
+function notaAnual(nota1, nota2, nota3){
+return Math.floor((nota1 + nota2 + nota3) / 3);
+}
+
+let resultFinal = notaAnual(5,10,7);
+
+if (resultFinal >= 7){
+    console.log("Aluno Aprovado");
+}else{ 
+    console.log("Aluno Reprovado");
+}
+
+
+*/
+
+/* 
+    2.  Crie uma função que converta o número do mês do ano pelo seu nome. 
+        Por exemplo:
+        Digite 07
+        retorne: Julho
+
+        Dica:
+        Nesse exercicio você vai usar a function e switch juntos.
+        
+        function mesAno(numero) {
+            case 01:
+                //return "Janeiro"
+                console.log("Janeiro");
+                break;
+            case 02:
+                //return "Fevereiro"
+                console.log("Fevereiro");
+                break;
+            case 03:
+                //return "Março"
+                console.log("Março");
+                break;
+            case 04:
+                //return "Abril"
+                console.log("Abril");
+                break;
+            case 05:
+                //return "Maio"
+                console.log("Maio");
+                break;
+            case 06:
+                //return "Junho"
+                console.log("Junho");
+                break;
+            case 07:
+                //return "Julho"
+                console.log("Julho");
+                break;
+            case 08:
+                //return "Agosto"
+                console.log("Agosto");
+                break;
+            case 09:
+                //return "Setembro"
+                console.log("Setembro");
+                break;
+            case 10:
+                //return "Outubro"
+                console.log("Outubro");
+                break;
+            case 11:
+                //return "Novembro"
+                console.log("Novembro");
+                break;
+            case 12:
+                //return "Dezembro"
+                console.log("Dezembro");
+                break;
+            default: console.log("Não é um mês");
+        }
+*/
+
+
+
