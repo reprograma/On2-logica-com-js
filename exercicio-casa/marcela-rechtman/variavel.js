099fe9292bd75d36c756fa5ba385564a6ab7c653
“MarcelaRecht$ git config --global user.name “MarcelaRecht <matz.marcela@gmail.com>
2019-11-22 12:38:04 -0300

    Adicionando exercicio casa Marcela Rechtman

---
+++ exercicio-casa/marcela-rechtman/variavel.js
@@ -0,0 +1,142 @@
+/*  
+   1. Crie as varias citadas abaixo, complentadno com suas informações e print no console.log seu retorno. 
+    
+        nome, profissão, escolaridade, estado, cidade
+
+    Dica: let IDENTIFICADOR = ATRIBUTO
+
+    
+
+*/
+
+
+let nome = "Marcela"
+undefined
+nome
+"Marcela"
+let profissao = "Designer"
+undefined
+profissao
+"Designer"
+let escolaridade = "Superior Completo"
+undefined
+let estado = "RJ"
+undefined
+let estado = console.log("RJ")
+VM10402:1 RJ
+undefined
+let cidade = console.log("Rio de Janeiro")
+VM10572:1 Rio de Janeiro
+undefined
+
+
+/*  
+    2. Com as variaveis criadas, substitua os valores já definidos por valores diferentes atribuidos a elas.
+
+        Dica: IDENTIFICADOR = ATRIBUTO
+    
+    obs: Lembrando que queremos substituir o valor atribuido, portanto, não é necessario declarar a variavel novamente. 
+
+*/
+nome = "Joana"
+"Joana"
+estado = "SP"
+"SP"
+profissao = "Programadora Front-End"
+"Programadora Front-End"
+escolaridade = "Superior Incompleto"
+"Superior Incompleto"
+cidade = "Sao Paulo"
+"Sao Paulo"
+
+/*  
+    3. Trabalhando variavel e logica, escreva:
+    
+        Variavel A = 10
+        Variavel B = 20
+
+    Troque o valor de A para B e valor de B para A.
+*/
+
+
+/*  
+    4. Escreva:
+
+        variavel A = 10
+        variavel B = 20
+        Mostre o valor de B
+        variavel B = 5
+        Mostre o valor de A e B
+*/
+> let A= 10
+undefined
+> let B = 20
+undefined
+> A = B
+20
+> B = A
+20
+>
+> console.log(A, B)
+20 20
+undefined
+>    
+
+/*  
+    5. Escreva:
+
+        variavel A = 30
+        variavel B = 20
+        variavel C = A + B
+        Mostre C
+        variavel B = 10
+        veriavel C = A + B
+        Mostre A,B,C
+*/
+
+
+/*  
+    6. Escreva:
+
+        variavel A = 10
+        variavel B = 20
+        variavel C = A 
+        variavel B = C
+        veriavel A = B
+        Mostre A,B,C
+*/
+> A = 10
+10
+> B = 20
+20
+> let C = A
+undefined
+> Let B = C
+Thrown:
+Let B = C
+> B = C
+10
+> A = B
+10
+> console.log (A, B, C)
+10 10 10
+undefined
+
+/*  
+    7. Crie uma constante A que armazene um valor de 100 e troque o valor de A para 80.
+
+    Responda:
+    Essa operação funcionou ? justifique
+*/Nao, Constante nao altera o valor 
+
+/*  
+    8. Um programa recebe um VALOR MONETÁRIO, e aplica uma TAXA de juros FIXA de 3% e no final apresenta o VALOR com a taxa aplicada. 
+    Onde usariamos varáveis, constante e console?
+*/
+
+let Valor = 1000
+undefined
+const Juros = 1.03
+undefined
+console.log(Valor*Juros)
+VM628:1 1030
