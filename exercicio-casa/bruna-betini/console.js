bed261a9d5de17f6ec8ccd7a1b4f71f8ac520d51
brunaBetini <bruna.betini@yahoo.com.br>
2019-11-22 23:42:22 -0300

    adicionando solucao de Bruna Betini

---
+++ exercicio-casa/bruna-betini/console.js
@@ -0,0 +1,16 @@
+/*
+
+Executar três operações matematicas, solicitadas abaixo:
+
+ subtração, divisão, porcentagem, multiplicação, adição
+
+*/
+
+let valor1 = 3
+let valor2 = 6
+
+let subtracao = Math.floor(valor1-valor2);
+let divisao = Math.floor(valor1/valor2);
+let porcentagem = Math.floor(valor1%valor2);
+let multiplicacao = Math.floor(valor1*valor2);
+let adicao = Math.floor(valor1+valor2);
