91759854e9dbe8e94e838dd2abc8b1198203535e
carolbedin <56762939+carolbedin@users.noreply.github.com>
2019-11-25 16:04:30 -0300

    carolina
    aula de js

---
+++ exercicio-casa/carolina/console.js
@@ -0,0 +1,20 @@
+/*
+
+Executar três operações matematicas, solicitadas abaixo:
+
+ subtração, divisão, porcentagem, multiplicação, adição
+
+*/
+
+let idadeMae = 60;
+let idadePai = 70;
+let soma = idadeMae + idadePai // 130
+
+let maracuja = 10;
+let abacaxi = 5;
+let subtracao = maracuja - abacaxi // 5
+
+let balas = 50
+let doces = 5
+let divisao = balas / doces //10
+//
---
+++ exercicio-casa/carolina/loop.js
@@ -0,0 +1,78 @@
+/*
+1.  Desenhe uma escada 5 andares, utilizando  um dos seguintes tipos de loop: for, do while ou while.
+    Utilize a string para passar dentro um caractere, para desenhar a escada.
+
+    Exemplo:
+    '#'
+    '##'
+    '###'
+    '####'
+    '#####'
+
+*/
+let andares = "#";
+
+for(i =1;i <= 5;i++){
+    console.log(andares)
+    andares = andares + "#";
+}
+/*
+ 2. Faça a taboada do 10 utilizando um dos seguintes loops for, while, ou do while.
+
+    Exemplo:
+
+    '1 x 10 = 10'
+    '2 x 10 = 20'
+    '3 x 10 = 30'
+    '4 x 10 = 40'
+    '5 x 10 = 50'
+    '6 x 10 = 60'
+    '7 x 10 = 70'
+    '8 x 10 = 80'
+    '9 x 10 = 90'
+    '10 x 10 = 100'
+*/
+
+
+
+for(let i =1;i <= 10;i++){
+    console.log(`${i} * 10 = ${i*10}`)
+    
+}
+
+
+
+
+/* 
+ 3. Faça um SWITCH para achar o dia da semana quirta-feira, resaltando que deve constar no seu código todos os dias da semana.
+
+*/
+
+let diaDaSemana = "quarta-feira";
+switch(diaDaSemana){
+    case "segunda-feira":
+        console.log("segunda-feira")
+    break;
+}
+let diaDaSemana = "quarta-feira";
+switch(diaDaSemana){
+    case "terça-feira":
+        console.log("terça-feira")
+    break;
+    case "quarta-feira":
+        console.log("quarta-feira")
+    break;
+    case "quinta-feira":
+        console.log("quinta-feira")
+    break;
+    case "sexta-feira":
+        console.log("sexta-feira")
+    break;
+    case "sabado-feira":
+        console.log("sabado")
+    break;
+    case "domingo-feira":
+        console.log("domingo")
+    break;
+    default: console.log("não é dia da semana")
+}
